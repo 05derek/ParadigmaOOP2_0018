1acad4ca699ec0ac8aaa82d9227ac66825186bee
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-02 11:12:27 +0700

    Pembuatan class lia yang turunan dari 'seseorang'

--- polymorphi/polymorphi.cpp
+++ polymorphi/polymorphi.cpp
@@ -15,3 +15,10 @@ class joko : public seseorang {
             cout << "Pesan dari Joko" << endl;
         }
 };
+
+class lia : public seseorang {
+    public :
+        void pesan() {
+            cout << "pesan dari lia" << endl;
+        }
+};
