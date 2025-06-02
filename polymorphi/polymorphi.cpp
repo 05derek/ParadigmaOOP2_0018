5bead96facb6252e4c93b12a99eec4417486e50e
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-02 11:11:02 +0700

    pembuatan class joko

--- polymorphi/polymorphi.cpp
+++ polymorphi/polymorphi.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 class seseorang {
     public :
-        virtual void pesan() = 0
+        virtual void pesan() = 0;
         //virtual void pesan(){
         // cout << "pesan dari seseorang" << endl;
         //}
+};
+
+class joko : public seseorang {
+    public :
+        void pesan() {
+            cout << "Pesan dari Joko" << endl;
+        }
 };
