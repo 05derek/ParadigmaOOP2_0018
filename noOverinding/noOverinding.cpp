fcd57606cf01596d459fc5f685774af6ea4abdbb
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-02 10:47:52 +0700

    int main dari noOverinding

--- noOverinding/noOverinding.cpp
+++ noOverinding/noOverinding.cpp
@@ -7,9 +7,17 @@ class baseClass {
             cout << "Hallo saya function dari base class";
         }
 };
-class deriveClass : public baseClass {
+class derivedClass : public baseClass {
     public :
         void perkenalan() {
             cout << "Hallo saya Function dari derived Class";
         }
 };
+
+int main() {
+    derivedClass a;
+
+    a.perkenalan();
+
+    return 0;
+}
